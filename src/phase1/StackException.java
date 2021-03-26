3cc501b399119a4479c47f4272fbb8043e67bfb4
coltonha <sha@my.canyons.edu>
2021-03-25 17:26:01 -0700

    first commit

---
+++ src/phase1/StackException.java
@@ -0,0 +1,7 @@
+package phase1;
+
+public class StackException extends java.lang.RuntimeException {
+    public StackException(String s) {
+        super(s);
+    } // end constructor
+} // end StackException
